0e42fb68e9a3ab4c1d12ef48169d49e2f1d8881c
97Fangz <briansegem25@gmail.com>
2023-08-11 02:36:59 -0700

    web3

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# wordpress website error fix
+
+exec { '/var/www/html/wp-setting.php':
+  command => "sed -i 's/.phpp/.php/g' /var/www/html/wp-settings.php",
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'
+}
